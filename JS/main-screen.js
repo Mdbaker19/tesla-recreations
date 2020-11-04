9b7a1296858939fba8d071fc571c01cba34200b9
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-11-04 13:08:48 -0600

    added map to display

--- JS/main-screen.js
+++ JS/main-screen.js
@@ -137,4 +137,13 @@
         setInterval(fadeActionOut, 2000);
     });
 
+    mapboxgl.accessToken = keyMap;
+    var mapObj = ({
+        container: "mapArea",//from id map in HTML
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [-98.4916, 29.4252], // starting position [lng, lat]
+        zoom: 10 , // starting zoom
+    });
+    var map = new mapboxgl.Map(mapObj);
+
 })();
